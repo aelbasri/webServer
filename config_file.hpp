9b0e8cbf0d28ad3e4320fd2ee24f25ac2248a478
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2025-01-21 19:22:29 +0100

    Refactor: Rename _location to location and add new methods

--- config_file.hpp
+++ config_file.hpp
@@ -6,7 +6,7 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 13:45:29 by zel-khad          #+#    #+#             */
-/*   Updated: 2025/01/21 16:21:52 by zel-khad         ###   ########.fr       */
+/*   Updated: 2025/01/21 19:19:32 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,19 +57,22 @@ class server : public error_pages
 {
 private:
     int     _nembre_of_location;
-    config_file *_location;
+    location *_location;
     std::string _content;
     std::string _name;
     std::string _host;
     std::string _port;
     std::string _max_body_size;
 public:
+    void new_location();
+    void Set_nembre_of_location(int _nembre_of_location);
     void Set_content(std::string __content);
     void Set_name(std::string __name);   
     void Set_host(std::string __host); 
     void Set_port(std::string __port);
     void Set_max_body_size(std::string __max_body_size); 
     
+    int Get_nembre_of_location();
     std::string Get_content();
     std::string Get_name();   
     std::string Get_host(); 
@@ -80,6 +83,8 @@ public:
     virtual ~server();
 
     void loadingDataserver(config_file Conf);
+    int CheckNumberOfLocation();
+    
 };
 
 
