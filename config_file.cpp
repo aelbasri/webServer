24c07ee09381c5412fcd6dfe69808008dcc802ac
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2025-01-21 13:07:57 +0100

    Refactor: Improve config file parsing and implementation
    - Rename functions to adhere to convention
    - Introduce function to retrieve number of servers
    - Remove empty lines from config file
    - Add missing newline to end of file

--- config_file.cpp
+++ config_file.cpp
@@ -6,7 +6,7 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 13:45:21 by zel-khad          #+#    #+#             */
-/*   Updated: 2025/01/20 17:52:30 by zel-khad         ###   ########.fr       */
+/*   Updated: 2025/01/21 13:06:49 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,6 @@ server::server(){}
 
 server::~server(){}
 
-config_file::config_file(){}
-
-config_file::~config_file(){}
-
-std::string& config_file::setFileContent(){
-    return(_fileContent);
-}
-
 void err(){
     std::cerr << "error " << std::endl;
     exit(1);
@@ -41,6 +33,8 @@ void err(){
 
 void loidingFile(config_file *Conf){
     string s;
+    std::string strCRLF = "\r\n";
+
     ifstream f("config_file.yaml");
 
     if (!f.is_open()) {
@@ -48,17 +42,60 @@ void loidingFile(config_file *Conf){
     }
     
     while (getline(f, s)){
+        s.erase(std::remove_if(s.begin(), s.end(), ::isspace),
+        s.end());
         Conf->setFileContent() += s;
-        Conf->setFileContent().push_back('\n');
+        if (!s.empty())
+            Conf->setFileContent().push_back('\n');
     }
     f.close();
 }
 
+
+config_file::config_file(){
+    loidingFile(this);
+    _nembre_of_server = CheckNumberOfServer();
+    _server =  new server[_nembre_of_server];
+}
+
+
+int config_file::get_nembre_of_server(){
+    return(_nembre_of_server);
+}
+
+config_file::~config_file(){
+    delete[] _server;
+}
+
+std::string& config_file::setFileContent(){
+    return(_fileContent);
+}
+
+
+int config_file::CheckNumberOfServer(){
+    std::string sentence = "server:";
+    int words = 0;
+    size_t pos = 0;
+    int lenOfSentence = sentence.size();
+    while (((pos = _fileContent.find(sentence, pos)) != (size_t)std::string::npos) && !sentence.empty()) {
+            pos += sentence.length();
+            words++;
+    }
+    return words;
+}
+
+
+void server::loadingDataserver(config_file Conf){
+    std::cout << Conf.setFileContent()<< std::endl;
+}
+
+
 int main() {
 
-    config_file Conf;
-    loidingFile(&Conf);
-    std::cout << Conf.setFileContent()<< std::endl;
+    config_file Conf; 
+
+    std::cout << Conf.get_nembre_of_server() << std::endl;
+    // Conf.loadingDataserver(Conf);
     
     return 0;
 }
