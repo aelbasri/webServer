e62cea8fbb353ce58ad2411802a32aad2bdc3baf
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2025-01-22 18:29:44 +0100

    Fix: Add getter and setter methods for error pages

--- configfile/error_pages.cpp
+++ configfile/error_pages.cpp
@@ -6,7 +6,7 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 10:50:26 by zel-khad          #+#    #+#             */
-/*   Updated: 2025/01/22 10:52:46 by zel-khad         ###   ########.fr       */
+/*   Updated: 2025/01/22 17:47:25 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,35 @@
 error_pages::error_pages(){}
 
 error_pages::~error_pages(){}
+
+void error_pages::SetForbiden(std::string __Forbidden){
+    _Forbidden = __Forbidden;
+}
+
+void error_pages::SetNotfound(std::string __NotFound){
+    _NotFound = __NotFound;
+}
+
+void error_pages::SetMethodNotAllowed(std::string __MethodNotAllowed){
+    _MethodNotAllowed = __MethodNotAllowed;
+}
+
+void error_pages::SetDefault(std::string __default){
+    _default = __default;
+}
+
+std::string error_pages::GetForbiddent(){
+    return _Forbidden;
+}
+
+std::string error_pages::GetNotFound(){
+    return _NotFound;
+}
+
+std::string error_pages::GetMethodNotAllowed(){
+    return _MethodNotAllowed;
+}
+
+std::string error_pages::GetDefault(){
+    return _default;
+}
