60b03316413050037bf1bb8e4a54f9e5261d73e7
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2025-01-23 16:47:02 +0100

    Refactor: Implement setters and getters for location class

--- configfile/location.hpp
+++ configfile/location.hpp
@@ -6,7 +6,7 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 10:53:03 by zel-khad          #+#    #+#             */
-/*   Updated: 2025/01/23 11:31:53 by zel-khad         ###   ########.fr       */
+/*   Updated: 2025/01/23 16:09:39 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,32 @@
 class location
 {
 private:
-    bool _autoindex;
-    std::string _index;
     std::string _type_of_location;
+    std::string _index;
     std::string _root_directory;
-    std::string _allowed_methods;
-    long long _max_body_size;
     std::string _upload_dir;
+    std::vector<std::string>  _allowed_methods;
+    long long _max_body_size;
     
 public:
     location();
     virtual ~location();
+
+
+    void SetType_of_location(std::string __type_of_location);
+    void SetIndex(std::string __index);
+    void SetRoot_directory(std::string __root_directory);
+    void SetAllowed_methods(int i, std::string target);
+    void SetUpload_dir(std::string __upload_dir);
+    void SetMax_body_size(long long __max_body_size);
+
+
+    std::string GetAllowed_methods(int i);
+    std::string GetType_of_location();    
+    std::string GetIndex();
+    std::string GetRoot_directory();    
+    std::string GetUpload_dir();
+    long long GetMax_body_size();
+    
+    // void loadContenLocation();
 };
