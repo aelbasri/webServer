d0dda022be33edaa3d5d47d95de87585a6e11071
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2025-01-24 21:06:47 +0100

    Refactor: Replace error pages with a map

--- configfile/error_pages.hpp
+++ configfile/error_pages.hpp
@@ -6,36 +6,32 @@
 /*   By: zel-khad <zel-khad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 10:50:32 by zel-khad          #+#    #+#             */
-/*   Updated: 2025/01/22 17:45:51 by zel-khad         ###   ########.fr       */
+/*   Updated: 2025/01/24 19:36:32 by zel-khad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <iostream>
 
 #include "error_pages.hpp"
+
+#include <iostream>
+#include <map>
 
 class error_pages
 {
 private:
-    std::string _Forbidden;
-    std::string _NotFound;
-    std::string _MethodNotAllowed;
+    std::map<std::string , std::string> _err;
     std::string _default;
 public:
     error_pages();
     virtual ~error_pages();
 
 
-    void SetForbiden(std::string __Forbidden);
-    void SetNotfound(std::string __NotFound);
-    void SetMethodNotAllowed(std::string __MethodNotAllowed);
+    void SetErr(std::map<std::string , std::string> __err);
     void SetDefault(std::string __default);
     
 
-    std::string GetForbiddent();
-    std::string GetNotFound();
-    std::string GetMethodNotAllowed();
+    std::map<std::string , std::string> GetErr();
     std::string GetDefault();
 };
