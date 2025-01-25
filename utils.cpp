bc58e44f8b0be895fedffdcc29591c2e3259c69e
saad-out <outsaad03@gmail.com>
2025-01-25 11:10:04 +0100

    change syntax

--- utils.cpp
+++ utils.cpp
@@ -1,38 +1,45 @@
 #include "Response.hpp"
 
+std::map<std::string, std::string> initializeMimeTypes() {
+    std::map<std::string, std::string> mimeTypes;
+    mimeTypes[".html"] = "text/html";
+    mimeTypes[".htm"] = "text/html";
+    mimeTypes[".css"] = "text/css";
+    mimeTypes[".js"] = "application/javascript";
+    mimeTypes[".json"] = "application/json";
+    mimeTypes[".xml"] = "application/xml";
+    mimeTypes[".txt"] = "text/plain";
+    mimeTypes[".jpg"] = "image/jpeg";
+    mimeTypes[".jpeg"] = "image/jpeg";
+    mimeTypes[".png"] = "image/png";
+    mimeTypes[".gif"] = "image/gif";
+    mimeTypes[".svg"] = "image/svg+xml";
+    mimeTypes[".ico"] = "image/vnd.microsoft.icon";
+    mimeTypes[".webp"] = "image/webp";
+    mimeTypes[".mp4"] = "video/mp4";
+    mimeTypes[".mpeg"] = "video/mpeg";
+    mimeTypes[".wav"] = "audio/wav";
+    mimeTypes[".mp3"] = "audio/mpeg";
+    mimeTypes[".ogg"] = "audio/ogg";
+    mimeTypes[".pdf"] = "application/pdf";
+    mimeTypes[".zip"] = "application/zip";
+    mimeTypes[".tar"] = "application/x-tar";
+    mimeTypes[".gz"] = "application/gzip";
+    mimeTypes[".csv"] = "text/csv";
+    return mimeTypes;
+}
+
 std::string getMimeType(const std::string& filename) {
-    static const std::map<std::string, std::string> mimeTypes = {
-        {".html", "text/html"},
-        {".htm", "text/html"},
-        {".css", "text/css"},
-        {".js", "application/javascript"},
-        {".json", "application/json"},
-        {".xml", "application/xml"},
-        {".txt", "text/plain"},
-        {".jpg", "image/jpeg"},
-        {".jpeg", "image/jpeg"},
-        {".png", "image/png"},
-        {".gif", "image/gif"},
-        {".svg", "image/svg+xml"},
-        {".ico", "image/vnd.microsoft.icon"},
-        {".webp", "image/webp"},
-        {".mp4", "video/mp4"},
-        {".mpeg", "video/mpeg"},
-        {".wav", "audio/wav"},
-        {".mp3", "audio/mpeg"},
-        {".ogg", "audio/ogg"},
-        {".pdf", "application/pdf"},
-        {".zip", "application/zip"},
-        {".tar", "application/x-tar"},
-        {".gz", "application/gzip"},
-        {".csv", "text/csv"}
-    };
+    static const std::map<std::string, std::string> mimeTypes = initializeMimeTypes();
 
     size_t dotPos = filename.rfind('.');
     if (dotPos != std::string::npos) {
         std::string extension = filename.substr(dotPos);
-        if (mimeTypes.find(extension) != mimeTypes.end())
-            return mimeTypes[extension];
+        std::map<std::string, std::string>::const_iterator it = mimeTypes.find(extension);
+        if (it != mimeTypes.end()) {
+            return it->second;
+        }
     }
     return "application/octet-stream";
 }
+
