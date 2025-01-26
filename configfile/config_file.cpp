cd808283e81d9e871ef09ee42164cdad11320286
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2025-01-26 13:21:17 +0100

    Refactor: Modify file handling and remove unused code
    - Modified `loadingDataserver` method in `config_file.cpp` to remove unnecessary function call.
    - Changed `loidingFile` method to take a string argument specifying the file to load.
    - Removed unused `main` function from `config_file.cpp`.

--- configfile/config_file.cpp
+++ configfile/config_file.cpp
@@ -13,38 +13,39 @@
 #include "config_file.hpp"
 
 void err(){
-    std::cerr << "error " << std::endl;
+    std::cerr << "error of open" << std::endl;
     exit(1);
 }
 
 
-void config_file::getServer(){
+server *config_file::getServer(){
+    return(_server);
 
-    std::map<std::string, std::string> mymap;
+    // std::map<std::string, std::string> mymap;
 
-    std::map<std::string, std::string>::iterator it;
-    for (size_t i = 0; i < _nembre_of_server; i++)
-    {
+    // std::map<std::string, std::string>::iterator it;
+    // for (size_t i = 0; i < _nembre_of_server; i++)
+    // {
         
-        std::cout << "---------------------------------sever n "<< i <<" ------------------------------------" << std::endl;
-    //    std::cout << _server[i].Get_content() << std::endl;
+    //     std::cout << "---------------------------------sever n "<< i <<" ------------------------------------" << std::endl;
+    // //    std::cout << _server[i].Get_content() << std::endl;
 
 
        
-       std::cout << "def host  :   " << _server[i].Get_host() << std::endl;
-       std::cout << "def name  :   " << _server[i].Get_name() << std::endl;
-       std::cout << "def max budy  :   " << _server[i].Get_max_body_size() << std::endl;
-       std::cout << "def port  :   " << _server[i].Get_port() << std::endl;
+    //    std::cout << "def host  :   " << _server[i].Get_host() << std::endl;
+    //    std::cout << "def name  :   " << _server[i].Get_name() << std::endl;
+    //    std::cout << "def max budy  :   " << _server[i].Get_max_body_size() << std::endl;
+    //    std::cout << "def port  :   " << _server[i].Get_port() << std::endl;
 
-       std::cout << "nembr of location is  :   " << _server[i].Get_nembre_of_location() << std::endl;
+    //    std::cout << "nembr of location is  :   " << _server[i].Get_nembre_of_location() << std::endl;
 
-        std::cout << "-------error_pages"<< i <<" ---------" << std::endl;
-       mymap = _server[i].GetErr();
-        for (it=mymap.begin(); it!=mymap.end(); ++it)
-          std::cout << it->first << " => " << it->second << '\n';
-    //    std::cout <<  "_default  : " <<_server[i].GetDefault() << std::endl;
+    //     std::cout << "-------error_pages"<< i <<" ---------" << std::endl;
+    //    mymap = _server[i].GetErr();
+    //     for (it=mymap.begin(); it!=mymap.end(); ++it)
+    //       std::cout << it->first << " => " << it->second << '\n';
+    // //    std::cout <<  "_default  : " <<_server[i].GetDefault() << std::endl;
 
-    }
+    // }
     
 }
 
@@ -69,17 +70,17 @@ void config_file::loadContentServer() {
             pos = nextServerPos;
         _server[i].Set_nembre_of_location(_server[i].CheckNumberOfLocation());
         _server[i].new_location();
-        _server[i].loadingDataserver(this);
+        _server[i].loadingDataserver();
     }
 
     
 }
 
-void loidingFile(config_file *Conf){
+void loidingFile(config_file *Conf, std::string file){
     string s;
     std::string strCRLF = "\r\n";
 
-    ifstream f("config_file.yaml");
+    ifstream f(file.c_str());
 
     if (!f.is_open()) {
         err();
@@ -93,8 +94,11 @@ void loidingFile(config_file *Conf){
     f.close();
 }
 
-config_file::config_file(){
-    loidingFile(this);
+
+config_file::config_file(){}
+
+config_file::config_file(std::string file){
+    loidingFile(this, file);
     _nembre_of_server = CheckNumberOfServer();
     _server =  new server[_nembre_of_server];
     
@@ -120,7 +124,6 @@ int config_file::CheckNumberOfServer(){
     std::string sentence = "server:";
     int words = 0;
     size_t pos = 0;
-    int lenOfSentence = sentence.size();
 
     std::string tmp;
 
@@ -133,18 +136,18 @@ int config_file::CheckNumberOfServer(){
     return words;
 }
 
-int main() {
-    try
-    {
-        config_file Conf; 
-        Conf.loadContentServer();
-        Conf.getServer();
+// int main() {
+//     try
+//     {
+//         config_file Conf; 
+//         Conf.loadContentServer();
+//         // Conf.getServer();
 
-     }
-     catch (runtime_error e)
-     {
-         cout << "L***A JAAAY!: " << e.what();
-     }
+//      }
+//      catch (runtime_error e)
+//      {
+//          cout << "L***A JAAAY!: " << e.what();
+//      }
 
-    return 0;
-}
+//     return 0;
+// }
