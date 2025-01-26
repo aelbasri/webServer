75217f23d5056fe010a89407d37548feed036404
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2025-01-26 13:21:34 +0100

    Refactor: Add socket-related methods for server configuration

--- configfile/server_data.hpp
+++ configfile/server_data.hpp
@@ -15,6 +15,18 @@
 #include "error_pages.hpp"
 #include "location.hpp"
 
+#include <fcntl.h>
+#include <iostream>
+#include <fstream>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <cstring>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 class server : public error_pages
 {
@@ -27,6 +39,12 @@ private:
     std::string _host;
     std::string _port;
     long long _max_body_size;
+
+    int         _sock;
+    struct addrinfo hints;
+    struct addrinfo *res ,*p;
+    int addI;
+
 public:
     void new_location();
     void Set_nembre_of_location(int _nembre_of_location);
@@ -34,7 +52,13 @@ public:
     void Set_name(std::string __name);   
     void Set_host(std::string __host); 
     void Set_port(std::string __port);
-    void Set_max_body_size(long long __max_body_size); 
+    void Set_max_body_size(long long __max_body_size);
+    void setSock(int sock);
+    void setRes(struct addrinfo* newRes);
+    void setP(struct addrinfo* newP);
+    void setAddI(int newAddI);
+    // void Set
+
     
     int Get_nembre_of_location();
     std::string Get_content();
@@ -42,6 +66,13 @@ public:
     std::string Get_host(); 
     std::string Get_port();
     long long Get_max_body_size();
+
+    int getSock() ;
+    struct addrinfo &getHints() ;
+    void setHints( struct addrinfo& newHints);
+    struct addrinfo* &getRes();
+    struct addrinfo* &getP();
+    int getAddI();
 
     server();
     virtual ~server();
@@ -51,7 +82,7 @@ public:
     void Getlocation();
     void LoidingAllowedMethods(std::vector<std::string> lines,size_t &i);
     void loadingLocationContent(std::vector<std::string> lines,size_t &i);
-    void loadingDataserver(config_file *Conf);
+    void loadingDataserver();
     int CheckNumberOfLocation();
     void loadingErrorIndex(std::vector<std::string> lines, size_t &i);
 };
