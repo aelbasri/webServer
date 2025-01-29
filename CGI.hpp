0bc4dc54bb2b7209049dd81d655547f712e3be72
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2025-01-29 12:42:10 +0100

    Add CGI class to handle CGI executables

---
+++ CGI.hpp
@@ -0,0 +1,23 @@
+
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <cstdio>
+#include <ctime>
+#include <sys/time.h>
+#include <sstream>
+
+class CGI
+{
+private:
+    std::string _PathOfExecutable;
+    std::string _type;
+    std::string _path;
+public:
+
+    void RunPythonCgi;
+    CGI();
+    ~CGI();
+};
+
