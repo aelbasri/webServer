d3a4f692bd54f4c176658406df6fb061c663c5ee
Zakariae El-Khadir <elkhadirzakaria48@gmail.com>
2025-01-29 12:42:09 +0100

    feat: Implement CGI python executable
    Resolves issues by generating a unique path for the CGI python executable.

---
+++ CGI.cpp
@@ -0,0 +1,49 @@
+#include "CGI.hpp"
+
+CGI::CGI(){}
+
+CGI::~CGI(){}
+
+std::string generate_unique_path() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    
+    std::stringstream ss;
+    ss << "/tmp/." << tv.tv_sec << tv.tv_usec << ".html";
+    return ss.str();
+}
+
+void   CGI::RunPythonCgi() {
+    std::string filename = generate_unique_path();
+    
+        std::stringstream  command = "python3 " + _path; 
+
+    FILE* pipe = popen(command, "r");
+    if (!pipe) {
+        std::cerr << "Error executing command." << std::endl;
+        return 1;
+    }
+
+    char buffer[128];
+    std::string result = "";
+    while (fgets(buffer, sizeof(buffer), pipe) != 0) {
+        result += buffer;
+    }
+
+    pclose(pipe);
+
+
+    std::ofstream file;
+    file.open(filename.c_str());
+
+    if (!file.is_open())
+    {
+        std::cout << "Error in creating file!" << std::endl;
+        return;
+    }
+    file << "<html><body><h1>Script Output</h1><pre>" << result << "</pre></body></html>";
+
+    _PathOfExecutable = filename;
+    return 0;
+}
+
